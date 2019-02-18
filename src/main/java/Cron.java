f8b34672e8cf00dcfbdc87e2279b66453a65dd9e
Alexandr <alexandr.fedorovgt@gmail.com>
2019-02-18 18:36:49 +0600

    fixed bugs

--- src/main/java/Cron.java
+++ src/main/java/Cron.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class Cron {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите запрос: 5 параметров разделенные пробелом.(минуты, часы, день месяца, месяц, день недели)");
         String[] userInput = scanner.nextLine().split(" ");
         while(userInput.length != 5) {
-            System.out.println("Введите корректный запрос");
+            System.out.println("Введите корректный запрос (необходимо ввести 5 параметров)");
             userInput = scanner.nextLine().split(" ");
         }
         Minute minute = new Minute();
--- src/main/java/Month.java
+++ src/main/java/Month.java
@@ -2,40 +2,39 @@ import java.util.Scanner;
 
 public class Month {
     String phraseForMonths(String month) {
-        if (month.equals("*")) {
-            return "каждый месяц, ";
-        }
-        if (!month.matches("[1-9]|12|11|10|\\*")) {
-            System.out.println("Введиете корректный месяц: ");
+        while (!month.matches("[1-9]|12|11|10|\\*")) {
+            System.out.println("Введиете корректный месяц(1-12): ");
             Scanner scanner = new Scanner(System.in);
             month = scanner.nextLine();
-            phraseForMonths(month);
+        }
+        if (month.equals("*")) {
+            return "каждый месяц,";
         }
         switch (month) {
             case "1":
-                return "в январе";
+                return "в январе,";
             case "2":
-                return "в феврале";
+                return "в феврале,";
             case "3":
-                return "в марте";
+                return "в марте,";
             case "4":
-                return "в апреле";
+                return "в апреле,";
             case "5":
-                return "в мае";
+                return "в мае,";
             case "6":
-                return "в июне";
+                return "в июне,";
             case "7":
-                return "в июле";
+                return "в июле,";
             case "8":
-                return "в августе";
+                return "в августе,";
             case "9":
-                return "в сентябре";
+                return "в сентябре,";
             case "10":
-                return "в октябре";
+                return "в октябре,";
             case "11":
-                return "в ноябре";
+                return "в ноябре,";
             case "12":
-                return "в декабре";
+                return "в декабре,";
             default:
                 return "месяц,";
         }
